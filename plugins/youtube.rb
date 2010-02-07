8913112633f0d8e6f3372e840273564433498c3d
maiha <maiha@wota.jp>
2010-02-08 07:03:02 +0900

    Check response.code for request

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -22,12 +22,31 @@ class YoutubePlugin < Ircbot::Plugin
 
       escaped = Rack::Utils.escape(url)
       http = Net::HTTP.new('localhost', 9111)
-      response = http.post('/register', "url=#{url}")
-      if response.code == '200'
+#      http = Net::HTTP.new('localhost', 3058)
+      tags = nick.to_s.gsub(/[0-9_]/,'')
+      response = http.post('/request', "url=#{url}&tags=#{tags}")
+      summary  = response.body.to_s.split(/\n/).first
+      case response.code
+      when '200'
+        return once("DL登録完了: #{summary}")
+      when '304'
+        return once("DL済: #{summary}")
+      else
+        reason = "%s: %s" % [response.code, summary]
+        return "DL登録失敗: #{reason}"
+      end
+    rescue Errno::ECONNREFUSED
+      return once("ダウンロードサーバが起動してません")
+    end
+
+    def once(text)
+      text = text.to_s.strip
+      @report_once ||= {}
+      if @report_once[text]
         return nil
       else
-        reason = "%s: %s" % [response.code, response.body.to_s.split(/\n/).first]
-        return "ダウンロード登録失敗: #{reason}"
+        @report_once[text] = Time.now
+        return text
       end
     end
 end
