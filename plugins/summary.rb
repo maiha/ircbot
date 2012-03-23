849bd03c83af8dba87ba577032cd4cd007a42d60
maiha <maiha@wota.jp>
2012-03-23 14:42:46 +0900

    plugin(summary) support https pages

--- plugins/summary.rb
+++ plugins/summary.rb
@@ -4,31 +4,68 @@ require 'rubygems'
 require 'ircbot'
 require 'uri'
 require 'ch2'
+require 'escape'
+
+######################################################################
+# [Install]
+#
+# gem install escape -V
+# apt-get install curl
+#
 
 class SummaryPlugin < Ircbot::Plugin
-  class Nop < RuntimeError; end
+  Nop    = Class.new(RuntimeError)
 
   def help
-    "[Summary] summarize web page (responds to only 2ch)"
+    "[Summary] summarize web page (responds to only 2ch or https)"
   end
 
   def reply(text)
-    url = parse_url(text)
-    return once(url) {summarize(url)}
+    scan_urls(text).each do |url|
+      case url
+      when /2ch\.net/
+        text = once(url) {summarize_2ch(url)}
+      when %r{^https:}
+        text = once(url) {summarize(url)}
+      else
+      end
+      done(text) if text
+    end
+    return nil
+
   rescue Nop
     return nil
   end
 
+  def trim_tags(html)
+    html.gsub!(%r{<script.*?>.*?</script>}mi, '')
+    html.gsub!(%r{<style.*?>.*?</style>}mi, '')
+    html.gsub!(%r{<noscript.*?>.*?</noscript>}mi, '')
+    html.gsub!(%r{</?.*?>}, '')
+    html.gsub!(/\s+/m, ' ')
+    return html
+  end
+
+  def summarize(url)
+    html = fetch(url)
+    html = trim_tags(html)
+    return html
+  end
+
+  def summarize_2ch(url)
+    dat = Ch2::Dat.new(url)
+    dat.valid? or raise Nop
+    return ">> %s" % trim_tags(dat.summarize)
+  end
+
   private
-    def parse_url(text)
-      uris = URI.extract(text).map{|i| i.sub(/^ttp:/, 'http:')}.grep(/2ch/)
-      uris.first or raise Nop
+    def scan_urls(text, &block)
+      URI.extract(text).map{|i| i.sub(/^ttp:/, 'http:')}
     end
 
-    def summarize(url)
-      dat = Ch2::Dat.new(url)
-      dat.valid? or raise Nop
-      return ">> %s" % dat.summarize.gsub(/\s+/m, ' ')
+    def fetch(url)
+      escaped_url = Escape.shell_command(url)
+      `curl '#{escaped_url}'`
     end
 
     def once(key, &block)
