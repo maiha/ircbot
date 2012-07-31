61690cf999a55bae4eff287f4b3a85f329372a73
maiha <maiha@wota.jp>
2012-07-31 13:44:20 +0900

    Added "engines" setting to summary config

--- plugins/summary/engines.rb
+++ plugins/summary/engines.rb
@@ -2,7 +2,9 @@ require 'dsl_accessor'
 require 'extlib'
 
 module Engines
-  Mapping = []
+  Mapping  = []
+  Loaded   = {}
+  NotFound = Class.new(RuntimeError)
 
   class NotImplementedError < NotImplementedError; end
   class Nop < NotImplementedError; end
@@ -14,17 +16,48 @@ module Engines
     raise NotImplementedError, "[BUG] Not supported URL: %s" % url
   end
 
-  # load ruby library and register its url
-  def self.register(name)
-    load File.dirname(__FILE__) + "/engines/#{name}.rb"
-    klass = instance_eval(Extlib::Inflection.camelize(name))
-    Mapping.unshift [klass.url, klass] unless klass == Base
+  def self.[](name)
+    unless loaded?(name)
+      raise NotFound, name.to_s
+    end
+    instance_eval(Extlib::Inflection.camelize(name))
   end
 
-  register("base")
-  register("none")
-  register("https")
-  register("ch2")
-  register("twitter")
+  def self.setup(*names)
+    Mapping.clear
+    names = (%w( none ) + names).flatten.uniq
+    debug "[Summary] Engines.setup(%s)" % names.inspect
+    names.each do |name|
+      klass = self[name]
+      Mapping.unshift [klass.url, klass]
+    end
+  end
+
+  # load ruby library
+  def self.load(name)
+    name = name.to_s
+    return false if loaded?(name)
+
+    file = File.dirname(__FILE__) + "/engines/#{name}.rb"
+    Kernel.load(file)
+
+    Loaded[name] = Time.now
+  end
+  def self.loaded?(name) Loaded[name]; end
+
+  def self.load_engines
+    load :base
+    load :none
+    Dir.glob(File.dirname(__FILE__) + "/engines/*.rb").sort.each do |file|
+      name = File.basename(file, ".*")
+      load(name)
+    end
+  end
+
+  # logger
+  def self.debug(msg)
+    puts msg.to_s
+  end
 end
 
+Engines.load_engines
--- plugins/summary/spec/summarizers_spec.rb
+++ plugins/summary/spec/summarizers_spec.rb
@@ -1,6 +1,24 @@
 require File.join(File.dirname(__FILE__), "spec_helper")
 
-describe Engines do
+describe Engines, "Engines with default settings" do
+  before { Engines.setup }
+
+  summary "http://www.asahi.com" do
+    its(:class) {should == Engines::None}
+  end
+end
+
+describe Engines, "Engines(http)" do
+  before { Engines.setup("http") }
+
+  summary "http://www.asahi.com" do
+    its(:class) {should == Engines::Http}
+  end
+end
+
+describe Engines, "Engines(https,twitter,ch2)" do
+  before { Engines.setup(["https", "twitter", "ch2"]) }
+
   summary "https://example.com" do
     its(:class) {should == Engines::Https}
   end
